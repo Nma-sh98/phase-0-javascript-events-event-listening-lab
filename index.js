1a582361b33ab5d3b95b1be9acce6622f97a270e
Nma Shawkat <nmashawket@gmail.com>
2022-11-14 12:23:10 +0300

    doneee

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//     alert('I was clicked!');
+// })
+
+// }
+
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', function () {
+        alert('Button was clicked!');
+    });
 }
+addingEventListener();
